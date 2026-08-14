9c1e193d579cc9949f58e6cc3b82ee8eca925f38
uday <udaykumar2600@gmail.com>
2026-08-15 01:53:39 +0530

    code editor create

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css"
+import { Editor } from "@monaco-editor/react"
+
 
 function App() {
 
 
   return (
     <>
-    <main
-    className="h-screen w-full bg-gray-950"
-    ></main>
+      <main
+        className="h-screen w-full bg-gray-950 flex gap-4 p-4"
+      >
+        <aside
+          className="h-full w-1/4 bg-amber-50 rounded-lg "
+        ></aside>
+        <section
+        className="w-3/4 bg-neutral-800 rounded-lg overflow-hidden">
+          <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          theme="vs-dark"
+          />
+        </section>
+      </main>
     </>
   )
 }
